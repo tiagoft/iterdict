e9649b63536169db46f1072ac7d878efece71d3a
Tiago Tavares <tiagoft@gmail.com>
2024-07-19 22:47:41 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iterdict",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # list of package dependencies
+    ],
+    author="Tiago F. Tavares",
+    author_email="tiagoft@gmail.com",
+    description="Use dictionaries to iterate over combinations of parameters"
+)
