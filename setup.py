4ce2125f8182a67593a4716c296efdbd27eb507c
Tiago Tavares <tiagoft@gmail.com>
2024-07-19 23:41:52 -0300

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="iterdict",
     version="0.1",
@@ -7,7 +10,10 @@ setup(
     install_requires=[
         # list of package dependencies
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Tiago F. Tavares",
     author_email="tiagoft@gmail.com",
-    description="Use dictionaries to iterate over combinations of parameters"
+    description="Use dictionaries to iterate over combinations of parameters",
+    url="https://github.com/tiagoft/iterdict"
 )
